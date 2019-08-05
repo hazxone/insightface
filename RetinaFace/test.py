753387e8d08ec5a07f5ad42dd90039b5889fcbcc
Hazwan <hazwan@gmail.com>
2019-08-05 11:41:12 +0800

    Edit for video test

--- RetinaFace/test.py
+++ RetinaFace/test.py
@@ -5,56 +5,95 @@ import datetime
 import os
 import glob
 from retinaface import RetinaFace
+import math
 
 thresh = 0.8
 scales = [1024, 1980]
+video_file = ''
+pad_ratio = 0.1
 
 count = 1
+frame_count = 0
 
 gpuid = 0
-detector = RetinaFace('./model/R50', 0, gpuid, 'net3')
+detector = RetinaFace('./models/R50', 0, gpuid, 'net3')
 
-img = cv2.imread('t1.jpg')
-print(img.shape)
-im_shape = img.shape
-target_size = scales[0]
-max_size = scales[1]
-im_size_min = np.min(im_shape[0:2])
-im_size_max = np.max(im_shape[0:2])
-#im_scale = 1.0
-#if im_size_min>target_size or im_size_max>max_size:
-im_scale = float(target_size) / float(im_size_min)
-# prevent bigger axis from being more than max_size:
-if np.round(im_scale * im_size_max) > max_size:
-    im_scale = float(max_size) / float(im_size_max)
+cam = cv2.VideoCapture(video_file)
 
-print('im_scale', im_scale)
+while True:
+  ret, img = cam.read()
+  frame_count += 1
+  print(frame_count)
+  # img = cv2.imread('sample-images/q2.png')
+  print(img.shape)
+  im_shape = img.shape
+  target_size = scales[0]
+  max_size = scales[1]
+  im_size_min = np.min(im_shape[0:2])
+  im_size_max = np.max(im_shape[0:2])
+  #im_scale = 1.0
+  #if im_size_min>target_size or im_size_max>max_size:
+  im_scale = float(target_size) / float(im_size_min)
+  # prevent bigger axis from being more than max_size:
+  if np.round(im_scale * im_size_max) > max_size:
+      im_scale = float(max_size) / float(im_size_max)
 
-scales = [im_scale]
-flip = False
+  # print('im_scale', im_scale)
 
-for c in range(count):
-  faces, landmarks = detector.detect(img, thresh, scales=scales, do_flip=flip)
-  print(c, faces.shape, landmarks.shape)
+  scales = [im_scale]
+  flip = False
 
-if faces is not None:
-  print('find', faces.shape[0], 'faces')
-  for i in range(faces.shape[0]):
-    #print('score', faces[i][4])
-    box = faces[i].astype(np.int)
-    #color = (255,0,0)
-    color = (0,0,255)
-    cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]), color, 2)
-    if landmarks is not None:
-      landmark5 = landmarks[i].astype(np.int)
-      #print(landmark.shape)
-      for l in range(landmark5.shape[0]):
-        color = (0,0,255)
-        if l==0 or l==3:
-          color = (0,255,0)
-        cv2.circle(img, (landmark5[l][0], landmark5[l][1]), 1, color, 2)
+  for c in range(count):
+    faces, landmarks = detector.detect(img, thresh, scales=scales, do_flip=flip)
+    # print(c, faces.shape, landmarks.shape)
 
-  filename = './detector_test.jpg'
-  print('writing', filename)
-  cv2.imwrite(filename, img)
+  if faces is not None:
+    # print('find', faces.shape[0], 'faces')
+    for i in range(faces.shape[0]):
+      #print('score', faces[i][4])
+      box = faces[i].astype(np.int)
+      #color = (255,0,0)
+      # color = (0,0,255)
+      # cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]), color, 2)
+      # print("Box {}_{},{},{},{}".format(i, box[0], box[1], box[2], box[3]))
+      width_face_init = box[2] - box[0]
+      height_face = box[3] - box[1]
+      padding = int(pad_ratio * height_face)
+      box_0 = box[0] - padding
+      box_1 = box[1] - padding
+      width_face = width_face_init + 2*padding
+      height_face = height_face + 2*padding
+
+      if landmarks is not None:
+        landmark5 = landmarks[i].astype(np.int)
+        #print(landmark.shape)
+        eye_left = landmark5[0]
+        eye_right = landmark5[1]
+        distance = (math.sqrt(((eye_left[0]-eye_right[0])**2)+((eye_left[1]-eye_right[1])**2)))/width_face_init
+        # print("Eye distance",distance)
+        # for l in range(landmark5.shape[0]):
+        #   color = (0,0,255)
+        #   if l==0 or l==1:
+        #     color = (0,255,0)
+        #   cv2.circle(img, (landmark5[l][0], landmark5[l][1]), 1, color, 2)
+
+      if distance > 0.3 and width_face >= 120:
+        img_crop = img[box_1:box_1+height_face, box_0:box_0+width_face]
+        cv2.imwrite('face_crop/cat_1/{}_{}.jpg'.format(frame_count,i), img_crop)
+
+      elif distance <= 0.3 and width_face > 120:
+        img_crop = img[box_1:box_1+height_face, box_0:box_0+width_face]
+        cv2.imwrite('face_crop/cat_2/{}_{}.jpg'.format(frame_count,i), img_crop)
+
+      else:
+        img_crop = img[box_1:box_1+height_face, box_0:box_0+width_face]
+        cv2.imwrite('face_crop/cat_3/{}_{}.jpg'.format(frame_count,i), img_crop)
+
+  if cv2.waitKey(1) == 27:
+      break  # esc to quit
+cv2.destroyAllWindows()
+
+    # filename = './detector_test.jpg'
+    # print('writing', filename)
+    # cv2.imwrite(filename, img)
 
--- gender-age/test.py
+++ gender-age/test.py
@@ -4,31 +4,46 @@ import cv2
 import sys
 import numpy as np
 import datetime
+import os
+import csv
 
 parser = argparse.ArgumentParser(description='face model test')
 # general
 parser.add_argument('--image-size', default='112,112', help='')
 parser.add_argument('--image', default='Tom_Hanks_54745.png', help='')
-parser.add_argument('--model', default='model/model,0', help='path to load model.')
+parser.add_argument('--model', default='gender-age/model/model,0', help='path to load model.')
 parser.add_argument('--gpu', default=0, type=int, help='gpu id')
 parser.add_argument('--det', default=0, type=int, help='mtcnn option, 1 means using R+O, 0 means detect from begining')
 args = parser.parse_args()
 
+gender_csv = open('face_crop/cat_1/gender_age_list.csv', "wb")
+writer = csv.writer(gender_csv, delimiter=",", quoting=csv.QUOTE_MINIMAL)
+
 model = face_model.FaceModel(args)
 #img = cv2.imread('Tom_Hanks_54745.png')
-img = cv2.imread(args.image)
-img = model.get_input(img)
-#f1 = model.get_feature(img)
-#print(f1[0:10])
-for _ in range(5):
+for im in os.listdir('./face_crop/cat_1')
+  img = cv2.imread(im)
+  # img = cv2.resize(img,(112,112))
+  img = model.get_input(img)
+  # f1 = model.get_feature(img)
+  # print(f1[0:10])
   gender, age = model.get_ga(img)
-time_now = datetime.datetime.now()
-count = 200
-for _ in range(count):
-  gender, age = model.get_ga(img)
-time_now2 = datetime.datetime.now()
-diff = time_now2 - time_now
-print('time cost', diff.total_seconds()/count)
-print('gender is',gender)
-print('age is', age)
+  if gender == 0:
+    gen = 'female'
+  else:
+    gen = 'male'
+  row =[im, gender, gen, age]
+  writer.writerow(row)
+  # for _ in range(5):
+  #   gender, age = model.get_ga(img)
+  # time_now = datetime.datetime.now()
+  # count = 200
+  # for _ in range(count):
+  #   gender, age = model.get_ga(img)
+  # time_now2 = datetime.datetime.now()
+  # diff = time_now2 - time_now
+  # print('time cost', diff.total_seconds()/count)
+  # print('gender is',gender)
+  # print('age is', age)
 
+gender_csv.close()
